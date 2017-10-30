9921934ccb15a1ab8f77a7c82199320843799c06
User <zipper@jacobzipper.com>
2017-10-30 02:32:46 -0400

    attempting auto queue

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 var alexa              = require('alexa-sdk');
 var constants          = require('./constants');
-var stateHandlers      = require('./stateHandlers');
-var audioEventHandlers = require('./audioEventHandlers');
+var stateHandlers      = require('./stateHandlers').stateHandlers;
+var audioEventHandlers = require('./stateHandlers').audioEventHandlers;
 var languageStrings    = require('./strings');
 
 exports.handler = (event, context, callback) => {
-    
+
 
     var skill = alexa.handler(event, context, callback);
-    
+
     skill.appId     = constants.appId;
     skill.resources = languageStrings;
     skill.debug     = constants.debug;
@@ -18,7 +18,7 @@ exports.handler = (event, context, callback) => {
         stateHandlers,
         audioEventHandlers
     );
-    
+
     if (skill.debug) {
         console.log("\n" + "******************* REQUEST **********************");
         console.log("\n" + JSON.stringify(event, null, 2));
--- stateHandlers.js
+++ stateHandlers.js
@@ -6,8 +6,7 @@ var request = require('request');
 var linkfinder = require('./youtube/lib/linkfinder');
 var knox = require('knox');
 var crypto = require('crypto');
-var songs = [];
-var cursor = 0;
+var users = {};
 var client = knox.createClient({
     key: process.env.aws_key
   , secret: process.env.aws_secret
@@ -29,6 +28,13 @@ var audioData = {
 
 var stateHandlers = {
     'GetVideoIntent': function () {
+        var devid = this.event.context.System.device.deviceId+"";
+        console.log(devid);
+        if (users[devid] == undefined) {
+            users[devid] = {};
+            users[devid]["songs"] = [];
+            users[devid]["cursor"] = 0;
+        }
         var slotVal = isSlotValid(this.event.request,"vid");
         var ye = this;
         if(slotVal == null) {
@@ -53,7 +59,7 @@ var stateHandlers = {
                         }
                         if(found) {
                             audioData.url = "https://s3.amazonaws.com/youtuberzipper/"+hash+".mp3";
-                            songs.push("https://s3.amazonaws.com/youtuberzipper/"+hash+".mp3");
+                            users[devid]["songs"].push("https://s3.amazonaws.com/youtuberzipper/"+hash+".mp3");
                             console.log(audioData.url);
                             ye.emit('PlayAudio');
                         }
@@ -78,6 +84,13 @@ var stateHandlers = {
         }
     },
     'QueueVideoIntent': function () {
+        var devid = this.event.context.System.device.deviceId+"";
+        console.log(devid);
+        if (users[devid] == undefined) {
+            users[devid] = {};
+            users[devid]["songs"] = [];
+            users[devid]["cursor"] = 0;
+        }
         var slotVal = isSlotValid(this.event.request,"vid");
         var ye = this;
         if(slotVal == null) {
@@ -102,7 +115,7 @@ var stateHandlers = {
                         }
                         if(found) {
                             audioData.url = "https://s3.amazonaws.com/youtuberzipper/"+hash+".mp3";
-                            songs.push("https://s3.amazonaws.com/youtuberzipper/"+hash+".mp3");
+                            users[devid]["songs"].push("https://s3.amazonaws.com/youtuberzipper/"+hash+".mp3");
                             console.log(audioData.url);
                             ye.response.speak("Added to your queue");
                             ye.emit(":responseReady");
@@ -131,8 +144,9 @@ var stateHandlers = {
     'PlayAudio': function () {
         // play the radio
         controller.play.call(this, this.t('WELCOME_MSG', { skillName: audioData.title } ));
-        cursor++;
-        console.log("played song " + (cursor -1));
+        var devid = this.event.context.System.device.deviceId+"";
+        users[devid]["cursor"] += 1;
+        console.log("played song " + (users[devid]["cursor"] - 1));
     },
     'AMAZON.HelpIntent': function () {
         this.response.listen(this.t('HELP_MSG', { skillName: audioData.title } ));
@@ -154,13 +168,26 @@ var stateHandlers = {
         this.emit('PlayAudio');
     },
     'AMAZON.PreviousIntent': function () {
-        cursor -= 2;
+        var devid = this.event.context.System.device.deviceId+"";
+        users[devid]["cursor"] -= 2;
         this.emit('PlayAudio');
     },
 
     'AMAZON.PauseIntent':   function () { this.emit('AMAZON.StopIntent'); },
     'AMAZON.CancelIntent':  function () { this.emit('AMAZON.StopIntent'); },
-    'AMAZON.StopIntent':    function () { controller.stop.call(this, this.t('STOP_MSG')) },
+    'AMAZON.StopIntent':    function () {
+        var devid = this.event.context.System.device.deviceId+"";
+        if (users[devid] == undefined) {
+            users[devid] = {};
+            users[devid]["songs"] = [];
+            users[devid]["cursor"] = 0;
+        }
+        else {
+            users[devid]["songs"] = [];
+            users[devid]["cursor"] = 0;
+        }
+        controller.stop.call(this, this.t('STOP_MSG'))
+    },
 
     'AMAZON.ResumeIntent':  function () { controller.play.call(this, this.t('RESUME_MSG')) },
 
@@ -175,10 +202,8 @@ var stateHandlers = {
         this.emit(':responseReady');
     },
     'AMAZON.StartOverIntent':  function () {
-        cursor--;
-        this.emit('PlayAudio');
-    },
-    'AudioPlayer.PlaybackFinished': function () {
+        var devid = this.event.context.System.device.deviceId+"";
+        users[devid]["cursor"] -= 1;
         this.emit('PlayAudio');
     },
 
@@ -189,7 +214,73 @@ var stateHandlers = {
     'PauseCommandIssued': function () { controller.stop.call(this, this.t('STOP_MSG')) }
 }
 
-module.exports = stateHandlers;
+var audioEventHandlers =  {
+    'PlaybackStarted' : function () {
+        /*
+         * AudioPlayer.PlaybackStarted Directive received.
+         * Confirming that requested audio file began playing.
+         * Do not send any specific response.
+         */
+        console.log("Playback started");
+        this.attributes['token'] = getToken.call(this);
+        this.attributes['index'] = getIndex.call(this);
+        this.attributes['playbackFinished'] = false;
+        this.emit(':responseReady');
+    },
+    'PlaybackFinished' : function () {
+        /*
+         * AudioPlayer.PlaybackFinished Directive received.
+         * Confirming that audio file completed playing.
+         * Do not send any specific response.
+         */
+        console.log("Playback finished");
+        this.emit(':responseReady');
+    },
+    'PlaybackStopped' : function () {
+        /*
+         * AudioPlayer.PlaybackStopped Directive received.
+         * Confirming that audio file stopped playing.
+         */
+        console.log("Playback stopped");
+
+        //do not return a response, as per https://developer.amazon.com/docs/custom-skills/audioplayer-interface-reference.html#playbackstopped
+        this.emit(':responseReady');
+    },
+    'PlaybackNearlyFinished' : function () {
+        /*
+         * AudioPlayer.PlaybackNearlyFinished Directive received.
+         * Replacing queue with the URL again.
+         * This should not happen on live streams
+         */
+        console.log("Playback nearly finished");
+        var enqueueIndex = this.attributes['index'];
+        enqueueIndex +=1;
+        this.attributes['enqueuedToken'] = String(this.attributes['playOrder'][enqueueIndex]);
+        var enqueueToken = this.attributes['enqueuedToken'];
+        var expectedPreviousToken = this.attributes['token'];
+        //this.response.audioPlayerPlay('REPLACE_ALL', audioData.url, audioData.url, null, 0);
+        var devid = this.event.context.System.device.deviceId+"";
+        console.log(devid);
+        if(users[devid] != undefined) {
+            console.log("enqueueing");
+            this.response.audioPlayerPlay('ENQUEUE', users[devid]["songs"][users[devid]["cursor"]], enqueueToken, expectedPreviousToken, 0);
+            users[devid]["cursor"] += 1;
+        }
+        this.emit(':responseReady');
+    },
+    'PlaybackFailed' : function () {
+        /*
+         * AudioPlayer.PlaybackFailed Directive received.
+         * Logging the error and restarting playing.
+         */
+        console.log("Playback Failed : %j", this.event.request.error);
+        this.response.audioPlayerClearQueue('CLEAR_ENQUEUED');
+        this.emit(':responseReady');
+    }
+};
+
+module.exports.stateHandlers = stateHandlers;
+module.exports.audioEventHandlers = audioEventHandlers;
 
 var controller = function () {
     return {
@@ -200,18 +291,26 @@ var controller = function () {
              *      Resuming audio when stopped/paused.
              *      Next/Previous commands issued.
              */
-            if(cursor < 0) {
-                cursor = 0;
-                this.response.speak("You requested to play a previous song when there are none. I will play the first song in the queue.").audioPlayerPlay('REPLACE_ALL', songs[cursor], songs[cursor], null, 0);
-            }
-            else if(cursor < songs.length) {
-                this.response.speak(text).audioPlayerPlay('REPLACE_ALL', songs[cursor], songs[cursor], null, 0);
+            var devid = this.event.context.System.device.deviceId+"";
+            if (users[devid] == undefined) {
+                this.response.speak("There was an error. Restart youtuber please.");
+                this.emit(":responseReady");
             }
             else {
-                this.response.speak("There are no more songs in your queue");
+                if(users[devid]["cursor"] < 0) {
+                    users[devid]["cursor"] = 0;
+                    this.response.speak("You requested to play a previous song when there are none. I will play the first song in the queue.").audioPlayerPlay('REPLACE_ALL', users[devid]["songs"][users[devid]["cursor"]], users[devid]["songs"][users[devid]["cursor"]], null, 0);
+                }
+                else if(users[devid]["cursor"] < users[devid]["songs"].length) {
+                    this.response.speak(text).audioPlayerPlay('REPLACE_ALL', users[devid]["songs"][users[devid]["cursor"]], users[devid]["songs"][users[devid]["cursor"]], null, 0);
+                }
+                else {
+                    users[devid]["cursor"] -= 1;
+                    this.response.speak("There are no more songs in your queue. Playing the last song.").audioPlayerPlay('REPLACE_ALL', users[devid]["songs"][users[devid]["cursor"]], users[devid]["songs"][users[devid]["cursor"]], null, 0);
 
+                }
+                this.emit(':responseReady');
             }
-            this.emit(':responseReady');
         },
         stop: function (text) {
             /*
@@ -252,4 +351,21 @@ function isSlotValid(request, slotName){
             return false;
         }
 }
+
+function getToken() {
+    // Extracting token received in the request.
+    return this.event.request.token;
+}
+
+function getIndex() {
+    // Extracting index from the token received in the request.
+    var tokenValue = parseInt(this.event.request.token);
+    return this.attributes['playOrder'].indexOf(tokenValue);
+}
+
+function getOffsetInMilliseconds() {
+    // Extracting offsetInMilliseconds received in the request.
+    return this.event.request.offsetInMilliseconds;
+}
+
 module.exports.getURL = getURL;
